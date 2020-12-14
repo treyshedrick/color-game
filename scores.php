8f34db05a44d7933380dcd13e7bc58a98c7e5de5
treyshedrick <treyshedrick@gmail.com>
2020-12-13 18:08:06 -0600

    add server side logic for top scores

---
+++ scores.php
@@ -0,0 +1,32 @@
+<?php
+include "credentials.php";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error)
+{
+  die("Unable to connect to database: " . $conn->connect_error);
+}
+
+//Query to display top scores
+$topscores = "SELECT * FROM Scores ORDER BY score desc  LIMIT 5";
+$result = $conn->query($topscores);
+
+if ($result->num_rows > 0)
+{
+  $scoretable = "<table><tr><td>Name</td><td>Score</td></tr>";
+  // Output highscores
+  while($row = $result->fetch_assoc())
+  {
+      $scoretable .= "<tr><td>".$row["name"]. "</td><td>" . $row["score"]. "</td></tr>";
+  }
+  $scoretable .= "</table>";
+  echo $scoretable;
+}
+else
+{
+  echo "No scores available";
+}
+
+$conn->close();
+?>
